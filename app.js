625c2ab092e1e731f4ec51f5b082bdb31c373618
uwindu <widucck21328@gmail.com>
2024-02-22 21:47:04 +0530

    create ejs template

--- app.js
+++ app.js
@@ -5,8 +5,41 @@ const bodyParser =require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/",function(req,res){
-    res.send("Hello");
+    
+    var today = new Date();
+    var currentday= today.getDay();
+    var day="";
+
+    switch(currentday) {
+        case 0:
+            day="Sunday";
+            break;
+        case 1:
+            day="Monday";
+            break;
+        case 2:
+            day="Tuesday";
+            break;
+        case 3:
+            day="Wednsday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+        default:
+          console.log("Error");
+      }
+
+    res.render("lists",{ kindOfDay :day});
 });
 
 app.listen(3000,function(){
