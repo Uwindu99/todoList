1635290794f97e633ba756911cf064f51351a51d
uwindu <widucck21328@gmail.com>
2024-02-23 12:37:06 +0530

    passing data

--- app.js
+++ app.js
@@ -5,42 +5,42 @@ const bodyParser =require("body-parser");
 
 const app = express();
 
+var items=["Buy food","Cook food","Eat food"];
+    
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 
 app.get("/",function(req,res){
     
     var today = new Date();
-    var currentday= today.getDay();
-    var day="";
+   
+    var options ={
+        weekday:"long",
+        day: "numeric",
+        month:"long"
 
-    switch(currentday) {
-        case 0:
-            day="Sunday";
-            break;
-        case 1:
-            day="Monday";
-            break;
-        case 2:
-            day="Tuesday";
-            break;
-        case 3:
-            day="Wednsday";
-            break;
-        case 4:
-            day="Thursday";
-            break;
-        case 5:
-            day="Friday";
-            break;
-        case 6:
-            day="Saturday";
-            break;
-        default:
-          console.log("Error");
-      }
+    };
 
-    res.render("lists",{ kindOfDay :day});
+    var day= today.toLocaleDateString("en-US",options)
+
+  
+
+    res.render("lists",{ kindOfDay :day,newList :items });
 });
+
+app.post("/",function(req,res){
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+
+    
+    
+})
+
 
 app.listen(3000,function(){
     console.log("server running with port 3000");
